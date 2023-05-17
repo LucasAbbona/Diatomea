8926525d6fa84321fa64e8529e1eae2b7163b9eb
Natalia Vega <103906510+nataliavega98@users.noreply.github.com>
2023-05-17 17:00:47 -0300

    colors theme tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+//  @type {import('tailwindcss').Config}
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      bg: {
+        page: "#FFFFFF",
+        navBar: "#FFFCF7"
+      },
+      primary:{
+        darkGrey: "#403FE",
+        grey: "#737272",
+        lightGrey: "#D9D6D2",
+        lighterGrey: "#BFBBB8",
+      },
+      secondary:{
+        darkBrown: "#C4A281",
+        brown: "#E2C3AA",
+        lightBrown: "#DAC3B1",
+        lighterBrown: "#C5A691",
+      },
+    },
   },
   plugins: [],
-}
-
+};
