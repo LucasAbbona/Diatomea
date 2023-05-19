c0a19162a4d4133c1543e45595c714a37560d262
LucasAbbona <lucasabbona0@gmail.com>
2023-05-19 16:57:25 -0300

    FAQ Page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import './index.css'
 import Footer from './components/Footer.jsx'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import QuienesSomos from './pages/QuienesSomos.jsx'
+import Faq from './pages/FAQ'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/QuienesSomos' element={<QuienesSomos />} />
+      <Route path='/FAQ' element={<Faq />} />
     </Routes>
     <Footer />
   </BrowserRouter>
