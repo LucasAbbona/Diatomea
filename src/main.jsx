4a5de3238e0caae740cdbb98db6c6aad41e2dcbf
LucasAbbona <lucasabbona0@gmail.com>
2023-05-19 15:49:27 -0300

    About Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import Footer from './components/Footer.jsx'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import QuienesSomos from './pages/QuienesSomos.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <Footer/>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/QuienesSomos' element={<QuienesSomos />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
 )
