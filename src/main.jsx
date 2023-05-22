0901d933d30111d495cdcdb4608f8a61392024e7
LucasAbbona <lucasabbona0@gmail.com>
2023-05-22 15:53:06 -0300

    Services Fixes and clean

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import Footer from './components/Footer.jsx'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import QuienesSomos from './pages/QuienesSomos.jsx'
 import Faq from './pages/FAQ'
+import Servicios from './pages/Servicios'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/QuienesSomos' element={<QuienesSomos />} />
       <Route path='/FAQ' element={<Faq />} />
+      <Route path='/Servicios' element={<Servicios />} />
     </Routes>
     <Footer />
   </BrowserRouter>
---
+++ src/pages/Servicios.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import Restauraciones from "../components/Restauraciones"
+import Limpieza from "../components/Limpieza"
+
+const Servicios = () => {
+    const [custom,setCustom] = useState(false)
+    const [reparaciones,setReparaciones] = useState(false)
+    const [grabado,setGrabado] = useState(false)
+    const [clean,setClean] = useState(false)
+    
+    return (
+        <div>
+            <div className='flex items-center flex-col my-10 '>
+                <p className='text-2xl font-medium text-primary-darkGrey mb-2'>Servicios</p>
+                <div className='h-0.5 w-52 bg-secondary-darkBrown mb-7'></div>
+                <p className="md:w-2/5 px-10 md:px-0 text-center text-lg text-primary-darkGrey">Acompáñanos a materializar tus emociones y momentos en una joya perfecta que te acompañará toda tu vida y será legado.</p>
+                <p className="md:w-1/3 px-5 md:px-0 text-center text-sm mt-7">Desde DIATOMEA te ofrecemos los siguiente servicios para que tu joya sea única y personalizada de manera artesanal. </p>
+            </div>
+            {/* Navegation */}
+            <div className="hidden md:flex gap-1 justify-center">
+                <button className={custom ?  "w-1/5 py-2 px-2.5 rounded-md border border-t-secondary-darkBrown text-primary-darkGrey text-lg font-medium bg-secondary-darkBrown":"w-1/5 py-2 px-2.5 rounded-md border border-t-secondary-darkBrown text-primary-darkGrey text-lg font-medium outline-secondary-darkBrown  ring-0"} onClick={()=>{setCustom(true);setClean(false);setGrabado(false);setReparaciones(false)}}>Joya personalizada</button>
+                <button className={reparaciones ? "w-1/5 py-2 px-2.5 rounded-md border border-t-secondary-darkBrown text-primary-darkGrey text-lg font-medium bg-secondary-darkBrown":"w-1/5 py-2 px-2.5 rounded-md border border-t-secondary-darkBrown text-primary-darkGrey text-lg font-medium outline-secondary-darkBrown  ring-0"} onClick={()=>{setCustom(false);setClean(false);setGrabado(false);setReparaciones(true)}}>Arreglo y restauracion</button>
+                <button className={grabado ? "w-1/5 py-2 px-2.5 rounded-md border border-t-secondary-darkBrown text-primary-darkGrey text-lg font-medium bg-secondary-darkBrown":"w-1/5 py-2 px-2.5 rounded-md border border-t-secondary-darkBrown text-primary-darkGrey text-lg font-medium outline-secondary-darkBrown  ring-0"} onClick={()=>{setCustom(false);setClean(false);setGrabado(true);setReparaciones(false)}}>Grabado</button>
+                <button className={clean ? "w-1/5 py-2 px-2.5 rounded-md border border-t-secondary-darkBrown text-primary-darkGrey text-lg font-medium bg-secondary-darkBrown":"w-1/5 py-2 px-2.5 rounded-md border border-t-secondary-darkBrown text-primary-darkGrey text-lg font-medium outline-secondary-darkBrown  ring-0"} onClick={()=>{setCustom(false);setClean(true);setGrabado(false);setReparaciones(false)}}>Limpieza</button>
+            </div>
+            {reparaciones && 
+            <Restauraciones />}
+            {clean && <Limpieza/>}
+            
+        </div>
+    )
+}
+
+export default Servicios
