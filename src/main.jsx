5104790493254ed54fbc884d7d94a7396288815d
Natalia <nataliacamilav@gmail.com>
2023-05-23 15:55:51 -0300

    semantic tags

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,23 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Footer from './components/Footer.jsx'
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import QuienesSomos from './pages/QuienesSomos.jsx'
-import Faq from './pages/FAQ'
-import Servicios from './pages/Servicios'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Footer from "./components/Footer.jsx";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import QuienesSomos from "./pages/QuienesSomos.jsx";
+import Faq from "./pages/FAQ";
+import Servicios from "./pages/Servicios";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes>
-      <Route path='/QuienesSomos' element={<QuienesSomos />} />
-      <Route path='/FAQ' element={<Faq />} />
-      <Route path='/Servicios' element={<Servicios />} />
-    </Routes>
+    <div className="App">
+      <header></header>
+      <main className="bg-bg-page">
+        <Routes>
+          <Route path="/QuienesSomos" element={<QuienesSomos />} />
+          <Route path="/FAQ" element={<Faq />} />
+          <Route path="/Servicios" element={<Servicios />} />
+        </Routes>
+      </main>
+    </div>
     <Footer />
   </BrowserRouter>
-)
+);
