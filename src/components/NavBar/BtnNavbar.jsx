1ddf0b940ed4721abe4e52c65d58d196485b1a35
LucasAbbona <lucasabbona0@gmail.com>
2023-05-26 20:20:27 -0300

    NavBar Linked to Services,FAQ and About page

--- src/components/NavBar/BtnNavbar.jsx
+++ src/components/NavBar/BtnNavbar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BtnNavbar = ({ text, icon, setCreations, setInfo, setServices, info, services, creations }) => {
   return (
     <>
@@ -8,12 +10,26 @@ const BtnNavbar = ({ text, icon, setCreations, setInfo, setServices, info, servi
           <li className='flex justify-center items-center gap-2.5 cursor-pointer md:text-base text-lg' onClick={() => { setInfo(prev => !prev); setCreations(false); setServices(false) }}>
             {text}{icon}
           </li> : text == "Servicios" ?
-            <li className='flex justify-center items-center gap-2.5 cursor-pointer md:text-base text-lg' onClick={() => { setServices(prev => !prev); setCreations(false); setInfo(false) }}>
-              {text}{icon}
-            </li> :
-            <li className='flex justify-center items-center gap-2.5 cursor-pointer md:text-base text-lg' >
-              {text}{icon}
-            </li>}
+            <Link to={'/Servicios'}>
+              <li className='flex justify-center items-center gap-2.5 cursor-pointer md:text-base text-lg' onClick={() => { setServices(prev => !prev); setCreations(false); setInfo(false) }}>
+                {text}{icon}
+              </li>
+            </Link> : text == 'Sobre Diatomea' ?
+              <Link to={'/QuienesSomos'}>
+                <li className='flex justify-center items-center gap-2.5 cursor-pointer md:text-base text-lg' >
+                  {text}{icon}
+                </li>
+              </Link> :
+              text == 'FAQ' ?
+              <Link to={'/FAQ'}>
+                <li className='flex justify-center items-center gap-2.5 cursor-pointer md:text-base text-lg' >
+                  {text}{icon}
+                </li>
+              </Link> :
+              <li className='flex justify-center items-center gap-2.5 cursor-pointer md:text-base text-lg' >
+                {text}{icon}
+              </li>
+        }
       </div>
     </>
   )
